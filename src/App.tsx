f5b7ec51daea2b534fb19f4ad9a862b3d99889e2
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-01-23 17:17:51 -0500

    Shadcn configs and UI system set up - installed button and calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState<Date | undefined>(new Date());
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>STARS AI Tutoring</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor et
+          commodi labore ipsum obcaecati! Explicabo, deleniti quaerat nisi
+          magnam quo eum adipisci officiis itaque earum, voluptas dolor
+          praesentium eligendi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor et
+          commodi labore ipsum obcaecati! Explicabo, deleniti quaerat nisi
+          magnam quo eum adipisci officiis itaque earum, voluptas dolor
+          praesentium eligendi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor et
+          commodi labore ipsum obcaecati! Explicabo, deleniti quaerat nisi
+          magnam quo eum adipisci officiis itaque earum, voluptas dolor
+          praesentium eligendi?
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
