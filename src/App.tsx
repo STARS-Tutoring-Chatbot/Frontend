0fc8d9525ae1b8c8c99cbe46a6a5808e3e3d2997
VincentCarrancho <71295634+VincentCarrancho@users.noreply.github.com>
2024-01-23 17:18:51 -0500

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [date, setDate] = useState<Date | undefined>(new Date());
-
   return (
     <>
       <div>
         <h1>STARS AI Tutoring</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor et
-          commodi labore ipsum obcaecati! Explicabo, deleniti quaerat nisi
-          magnam quo eum adipisci officiis itaque earum, voluptas dolor
-          praesentium eligendi?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor et
-          commodi labore ipsum obcaecati! Explicabo, deleniti quaerat nisi
-          magnam quo eum adipisci officiis itaque earum, voluptas dolor
-          praesentium eligendi?
-        </p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolor et
           commodi labore ipsum obcaecati! Explicabo, deleniti quaerat nisi
