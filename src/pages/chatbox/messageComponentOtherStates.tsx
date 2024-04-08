ac2bb124c769c15a0af5c20ef472304c2a6f7b80
kneesknees <thereisapaperonthefloor@gmail.com>
2024-04-07 21:14:01 -0400

    message load and error

--- src/pages/chatbox/messageComponentOtherStates.tsx
+++ src/pages/chatbox/messageComponentOtherStates.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { AlertTriangle } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+
 
 type MessageComponentOtherStatesProps = {
   isError: boolean;
@@ -14,15 +17,44 @@ function MessageComponentOtherStates({
 }: MessageComponentOtherStatesProps) {
   // Loading State
   if (isLoading) {
-    // create loading component
-    return <div>LOADING</div>;
+    // Return a skeleton loading component
+  return (
+    <div className="flex justify-between items-center w-full">
+      <div className = "w-full">
+        <div className="text-xl font-semibold mb-2 px-4">Assistant</div>
+        <div className="space-y-2 w-full">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+        </div>
+      </div>
+    </div>
+    );
   }
   // error state
   else if (isError) {
     if (errorMessage === "") {
       errorMessage = "Something went wrong.";
     }
-    return <div>ERROR</div>;
+    const reload = () => {
+      window.location.reload();
+    };
+    return (
+      <div className="w-full flex justify-center items-center">
+      <div className="flex items-center">
+        <AlertTriangle className="text-destructive w-10 h-10" />
+        <div className="flex flex-col ml-2">
+          <div className="text-destructive text-center">
+            Error: {errorMessage}
+          </div>
+          <Button className="text-sm" variant={"link"} onClick={reload}>
+            Please reload the page
+          </Button>
+        </div>
+      </div>
+    </div>
+    );
   }
 }
 
